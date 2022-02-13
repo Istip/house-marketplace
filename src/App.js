7a41bd765e087e089db857c4bd45c5023f2f6516
Istip <isticsek@gmail.com>
2022-02-13 17:05:30 +0200

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+// eslint-disable-next-line
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
+  );
 }
 
 export default App;
