857e02535801b5217c3cb6fa91c4df074cbee3d9
Istip <isticsek@gmail.com>
2022-02-14 21:32:15 +0200

    private route and hook for authentication status added

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStatus } from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
+
+const PrivateRoute = () => {
+  const { loading, loggedIn } = useAuthStatus();
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+};
+
+export default PrivateRoute;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,23 +1,99 @@
-import React, { useEffect, useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useNavigate, Link } from 'react-router-dom';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
 
 const Profile = () => {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-    // eslint-disable-next-line
-  }, []);
+  const [changeDetails, setChangeDetails] = useState(false);
 
-  if (!user) {
-    return <div>Not logged in!</div>;
-  }
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  // Functions
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update the display name if it is not the same as it was
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // Update in the firestore db
+        const userRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(userRef, { name });
+      }
+    } catch (error) {
+      toast.error('Could not update profile details! Try again later!');
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
   return (
-    <div>
-      <h1>{user.displayName}</h1>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+
+        <button type="button" onClick={onLogout} className="logOut">
+          Log Out
+        </button>
+      </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails(!changeDetails);
+            }}
+          >
+            {changeDetails ? 'Done' : 'Change'}
+          </p>
+        </div>
+
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+              required
+            />
+
+            <input
+              type="email"
+              id="email"
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
