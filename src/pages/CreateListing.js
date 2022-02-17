e38f6d4eae016d6f3acd82051b68a7d7d01b9704
Istip <isticsek@gmail.com>
2022-02-17 22:56:36 +0200

    fixed not workingness :D

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,7 +1,4 @@
 import { useState, useEffect, useRef } from 'react';
-import { toast } from 'react-toastify';
-import { v4 as uuidv4 } from 'uuid';
-import { useNavigate } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import {
   getStorage,
@@ -11,10 +8,13 @@ import {
 } from 'firebase/storage';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
-
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
 import Spinner from '../components/Spinner';
 
-const CreateListing = () => {
+function CreateListing() {
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(false);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -32,6 +32,7 @@ const CreateListing = () => {
     latitude: 0,
     longitude: 0,
   });
+
   const {
     type,
     name,
@@ -50,10 +51,25 @@ const CreateListing = () => {
 
   const auth = getAuth();
   const navigate = useNavigate();
-
   const isMounted = useRef(true);
 
-  // Functions
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid });
+        } else {
+          navigate('/sign-in');
+        }
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMounted]);
+
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -61,13 +77,13 @@ const CreateListing = () => {
 
     if (discountedPrice >= regularPrice) {
       setLoading(false);
-      toast.error('Discounted price needs to be less then the regular price!');
+      toast.error('Discounted price needs to be less than regular price');
       return;
     }
 
     if (images.length > 6) {
       setLoading(false);
-      toast.error('Max 6 images can be uploaded!');
+      toast.error('Max 6 images');
       return;
     }
 
@@ -75,7 +91,9 @@ const CreateListing = () => {
     let location;
 
     if (geolocationEnabled) {
-      const response = await fetch(``);
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
 
       const data = await response.json();
 
@@ -89,7 +107,7 @@ const CreateListing = () => {
 
       if (location === undefined || location.includes('undefined')) {
         setLoading(false);
-        toast.error('Please enter a correct address!');
+        toast.error('Please enter a correct address');
         return;
       }
     } else {
@@ -97,12 +115,14 @@ const CreateListing = () => {
       geolocation.lng = longitude;
     }
 
-    // store images in firebase
+    // Store image in firebase
     const storeImage = async (image) => {
       return new Promise((resolve, reject) => {
         const storage = getStorage();
         const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
-        const storageRef = ref(storage, `images/${fileName}`);
+
+        const storageRef = ref(storage, 'images/' + fileName);
+
         const uploadTask = uploadBytesResumable(storageRef, image);
 
         uploadTask.on(
@@ -111,8 +131,6 @@ const CreateListing = () => {
             const progress =
               (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             console.log('Upload is ' + progress + '% done');
-
-            // eslint-disable-next-line
             switch (snapshot.state) {
               case 'paused':
                 console.log('Upload is paused');
@@ -120,12 +138,16 @@ const CreateListing = () => {
               case 'running':
                 console.log('Upload is running');
                 break;
+              default:
+                break;
             }
           },
           (error) => {
             reject(error);
           },
           () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
               resolve(downloadURL);
             });
@@ -134,11 +156,12 @@ const CreateListing = () => {
       });
     };
 
-    const imgUrls = Promise.all(
+    const imgUrls = await Promise.all(
       [...images].map((image) => storeImage(image))
     ).catch(() => {
       setLoading(false);
-      toast.error('Images could not be uploaded!');
+      toast.error('Images not uploaded');
+      return;
     });
 
     const formDataCopy = {
@@ -154,11 +177,9 @@ const CreateListing = () => {
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
-
     setLoading(false);
-    toast.success('Listing saved!');
-
-    navigate.to(`/category/${formDataCopy.type}/${docRef.id}`);
+    toast.success('Listing saved');
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
   };
 
   const onMutate = (e) => {
@@ -167,7 +188,6 @@ const CreateListing = () => {
     if (e.target.value === 'true') {
       boolean = true;
     }
-
     if (e.target.value === 'false') {
       boolean = false;
     }
@@ -180,7 +200,7 @@ const CreateListing = () => {
       }));
     }
 
-    // Text / Booleans / Numbers
+    // Text/Booleans/Numbers
     if (!e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
@@ -188,19 +208,6 @@ const CreateListing = () => {
       }));
     }
   };
-
-  useEffect(() => {
-    if (isMounted) {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setFormData({ ...formData, userRef: user.uid });
-        } else navigate('/sign-in');
-      });
-    }
-
-    return () => (isMounted.current = false);
-    // eslint-disable-next-line
-  }, [isMounted]);
 
   if (loading) {
     return <Spinner />;
@@ -442,6 +449,6 @@ const CreateListing = () => {
       </main>
     </div>
   );
-};
+}
 
 export default CreateListing;
